ccb6b155cee7f371440ff7def6afd8acbcc7c858
Rakesh Purohit <98805545+rakesh-purohit9@users.noreply.github.com>
2022-12-16 13:23:07 +0530

    copy to clipboard

--- src/constants/eventdetails.js
+++ src/constants/eventdetails.js
@@ -2,8 +2,3 @@ export const event = {
   handle: 'awsugahm',
   hashtags: "AWSCommunityDay,BuildWithDhiWise💻,ACDAhm2022"
 }
-
-// export const event = {
-//     handle: 'GDGPune',
-//     hashtags: "#GDGPune2022 #DevFestPune2022 #BuildWithDhiWise💻"
-// }
