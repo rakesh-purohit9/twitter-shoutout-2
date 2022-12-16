f3effb389e740b09d0a4fe3ef1b9ed9b8b14c772
Rakesh Purohit <rakesh.purohit@dhiwise.com>
2022-12-16 11:44:05 +0530

    base set

--- src/pages/Giveaway/index.js
+++ src/pages/Giveaway/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-import { Column, Row, Img, Text, Grid, Stack } from "components";
-import TweetCard from "components/TweetCard";
+import { Column, Row, Img, Text, Grid } from "components";
+import TweetCard from "../../components/TweetCard";
+import { DhiWiseTweets } from "../../constants/tweets";
 
 const GiveawayPage = () => {
   return (
     <>
-      <Column className="bg-black_900 flex flex-col font-gilroy items-center justify-start mx-[auto] sm:p-[15px] md:p-[24px] p-[36px] w-[100%]">
-        <Column className="flex flex-col items-center justify-start max-w-[1241px] sm:mb-[14px] md:mb-[19px] mb-[28px] ml-[auto] mr-[auto] sm:pl-[15px] sm:pr-[15px] w-[100%]">
-          <Row className="flex sm:flex-col flex-row md:flex-wrap sm:flex-wrap items-center sm:items-center justify-between sm:justify-center sm:px-[0] w-[100%]">
+      <Column className="bg-black_900 flex flex-col font-sans items-center justify-start mx-[auto] sm:p-[15px] md:p-[24px] p-[36px] w-[100%]">
+        <Column className="flex flex-col items-center justify-start max-w-[1241px] sm:mb-[14px] md:mb-[19px] mb-[28px] mx-[auto] sm:px-[15px] w-[100%]">
+          <Row className="flex flex-row sm:flex-col md:flex-wrap sm:flex-wrap items-center justify-between sm:px-[0] w-[100%]">
             <Img
               src="images/img_group.svg"
               className="flex-shrink-0 max-w-[100%] w-[14%] sm:w-[50%]"
@@ -22,78 +23,54 @@ const GiveawayPage = () => {
               DhiWise x AWS Ahmedabad Shoutout
             </Text>
           </Row>
-          <Column className="flex flex-col items-center justify-start sm:mt-[34px] md:mt-[44px] mt-[64px] w-[100%]">
+          <Column className="flex flex-col items-center justify-start sm:mt-[34px] md:mt-[44px] mt-[64px] w-[100%] sm:mb-16">
             <Grid className="sm:gap-[14px] md:gap-[19px] gap-[28px] grid sm:grid-cols-1 md:grid-cols-2 grid-cols-3 min-h-[auto] w-[100%]">
-              <TweetCard
-                className="border border-gray_900 border-solid flex flex-col justify-start md:p-[11px] p-[16px] sm:px-[15px] sm:py-[8px] rounded-radius12 w-[100%]"
-                hashtag="#AppleMagicMouseGiveAwayContest"
-                desc="Quote this tweet with your picture with our #Flutter props ar DhiWise booth at @flutterconfin today and stand a chance to win an Apple Magic Mouse! Head to this link for the T&C."
-              />
-              <TweetCard
-                className="border border-gray_900 border-solid flex flex-col justify-start md:p-[11px] p-[16px] sm:px-[15px] sm:py-[8px] rounded-radius12 w-[100%]"
-                hashtag="#AppleMagicMouseGiveAwayContest"
-                desc="Quote this tweet with your picture with our #Flutter props ar DhiWise booth at @flutterconfin today and stand a chance to win an Apple Magic Mouse! Head to this link for the T&C."
-              />
-              <TweetCard
-                className="border border-gray_900 border-solid flex flex-col justify-start md:p-[11px] p-[16px] sm:px-[15px] sm:py-[8px] rounded-radius12 w-[100%]"
-                hashtag="#AppleMagicMouseGiveAwayContest"
-                desc="Quote this tweet with your picture with our #Flutter props ar DhiWise booth at @flutterconfin today and stand a chance to win an Apple Magic Mouse! Head to this link for the T&C."
-              />
-              <TweetCard
-                className="border border-gray_900 border-solid flex flex-col justify-start md:p-[11px] p-[16px] sm:px-[15px] sm:py-[8px] rounded-radius12 w-[100%]"
-                hashtag="#AppleMagicMouseGiveAwayContest"
-                desc="Quote this tweet with your picture with our #Flutter props ar DhiWise booth at @flutterconfin today and stand a chance to win an Apple Magic Mouse! Head to this link for the T&C."
-              />
-              <TweetCard
-                className="border border-gray_900 border-solid flex flex-col justify-start md:p-[11px] p-[16px] sm:px-[15px] sm:py-[8px] rounded-radius12 w-[100%]"
-                hashtag="#AppleMagicMouseGiveAwayContest"
-                desc="Quote this tweet with your picture with our #Flutter props ar DhiWise booth at @flutterconfin today and stand a chance to win an Apple Magic Mouse! Head to this link for the T&C."
-              />
-              <TweetCard
-                className="border border-gray_900 border-solid flex flex-col justify-start md:p-[11px] p-[16px] sm:px-[15px] sm:py-[8px] rounded-radius12 w-[100%]"
-                hashtag="#AppleMagicMouseGiveAwayContest"
-                desc="Quote this tweet with your picture with our #Flutter props ar DhiWise booth at @flutterconfin today and stand a chance to win an Apple Magic Mouse! Head to this link for the T&C."
-              />
+              {DhiWiseTweets?.length > 0 ? DhiWiseTweets.map(tweetRecord => (<TweetCard key={tweetRecord?.id} tweetData={tweetRecord} />)) : "DhiWise is preparing for upcoming events"}
             </Grid>
           </Column>
-          <Row className="flex flex-row md:flex-wrap sm:flex-wrap sm:items-center items-end justify-between sm:justify-center mt-[120px] sm:mt-[63px] md:mt-[82px] sm:px-[0] w-[100%]">
+          <Row className="flex flex-row sm:justify-center sm:space-y-4 md:flex-wrap sm:flex-wrap items-end justify-between mt-[120px] sm:mt-[63px] md:mt-[82px] w-[100%] fixed bottom-0 bg-black_900 p-2">
             <Text
-              className="font-normal sm:m-[5px] mb-[2px] md:mt-[4px] mt-[6px] not-italic sm:p-[5px] sm:text-[21px] text-white_A700 w-[auto]"
+              className="font-normal mb-[2px] sm:mt-[3px] md:mt-[4px] mt-[6px] not-italic text-white_A700 w-[auto]"
               as="h3"
               variant="h3"
             >
-              Powered by DhiWIse
+              Generated with ❤️ from DhiWise
             </Text>
-            <Row className="flex flex-row md:flex-wrap sm:flex-wrap items-center justify-between sm:mx-[0] sm:px-[0] sm:w-[100%] w-[22%]">
-              <Img
+            <Row className="flex flex-row md:flex-wrap sm:flex-wrap items-center justify-between sm:justify-center space-x-8 sm:mx-[0] sm:px-[0] sm:w-[100%] w-[22%]">
+              {/* <Img
                 src="images/img_image1.png"
                 className="sm:h-[13px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[12px] md:w-[16px] w-[24px]"
-                alt="imageOne"
-              />
+                alt="facebook"
+              /> */}
               <Img
                 src="images/img_image2.png"
-                className="sm:h-[13px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[12px] md:w-[16px] w-[24px]"
-                alt="imageTwo"
+                className="sm:h-[30px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[30px] md:w-[16px] w-[24px] cursor-pointer"
+                alt="Twitter"
+                onClick={() => window.open('https://twitter.com/DhiWise')}
               />
               <Img
                 src="images/img_image3.png"
-                className="sm:h-[13px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[12px] md:w-[16px] w-[24px]"
-                alt="imageThree"
+                className="sm:h-[30px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[30px] md:w-[16px] w-[24px] cursor-pointer"
+                alt="LinkedIn"
+                onClick={() => window.open('https://linkedin.com/company/DhiWise')}
               />
               <Img
                 src="images/img_image6.png"
-                className="sm:h-[13px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[12px] md:w-[16px] w-[24px]"
-                alt="imageSix"
+                className="sm:h-[30px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[30px] md:w-[16px] w-[24px] cursor-pointer"
+                alt="GitHub"
+                onClick={() => window.open('https://github.com/DhiWise')}
               />
               <Img
                 src="images/img_image5.png"
-                className="sm:h-[13px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[12px] md:w-[16px] w-[24px]"
-                alt="imageFive"
+                className="sm:h-[30px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[30px] md:w-[16px] w-[24px] cursor-pointer"
+                alt="YouTube"
+                onClick={() => window.open('https://youtube.com/@DhiWise')}
               />
               <Img
                 src="images/img_image4.png"
-                className="sm:h-[13px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[12px] md:w-[16px] w-[24px]"
-                alt="imageFour"
+                className="sm:h-[30px] md:h-[17px] h-[24px] max-w-[100%] sm:w-[30px] md:w-[16px] w-[24px] cursor-pointer"
+                alt="Instagram"
+                onClick={() => window.open('https://instagram.com/DhiWise')}
               />
             </Row>
           </Row>
